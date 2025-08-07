c9d72430f16dba562248d4b56738338798919803
rhowlader5750 <rhowlader2001@gmail.com>
2025-08-07 00:20:03 -0400

    spring init

---
+++ bank-ledger-app/src/main/java/com/example/bank_ledger_app/BankLedgerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_ledger_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLedgerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankLedgerAppApplication.class, args);
+	}
+
+}
---
+++ bank-ledger-app/src/test/java/com/example/bank_ledger_app/BankLedgerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_ledger_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLedgerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
